0d9a53500f1cf2d33b0798bc6132a2335a1ddd52
pratap <pekorge@gmail.com>
2024-03-12 13:16:51 +0530

    user management project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UserlistComponent} from "./userlist/userlist.component";
+import {UserAddComponent} from "./user-add/user-add.component";
+import {UserUpdateComponent} from "./user-update/user-update.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+   component: UserlistComponent,
+    pathMatch: 'full'
+  },
+  {
+    path:'user-add',
+    component: UserAddComponent,
+
+  },
+  {
+    path: 'user-update/:id',
+    component: UserUpdateComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,48 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserlistComponent } from './userlist/userlist.component';
+import { UserAddComponent } from './user-add/user-add.component';
+import { HeaderComponent } from './header/header.component';
+import { DrawerComponent } from './drawer/drawer.component';
+// import {MatCardModule} from "@angular/material/card";
+// import {MatIconModule} from "@angular/material/icon";
+//  import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { UserUpdateComponent } from './user-update/user-update.component';
+import {HttpClientModule} from "@angular/common/http";
+import {UserService} from "./user.service";
+import {AppconstantService} from "./appconstant.service";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserlistComponent,
+    UserAddComponent,
+    HeaderComponent,
+    DrawerComponent,
+    UserUpdateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // MatCardModule,
+    // MatIconModule
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    BrowserAnimationsModule
+
+
+
   ],
-  providers: [],
+  providers: [UserService, AppconstantService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
